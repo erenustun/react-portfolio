86b99b28ab58c51680368ea959c65d019345f7ae
Eren <eren.uestuen@gmail.com>
2020-10-12 01:33:48 +0200

    Added social links

--- pages/header/index.js
+++ pages/header/index.js
@@ -5,24 +5,26 @@ export default function Header() {
         <header className="d-flex align-items-center">
             <div className="d-flex w-75 mx-auto align-items-center">
                 <h1 className="flex-grow-1 text-text-left m-0 mb-4 display-4 text-white">Eren.develops</h1>
-                <ul className="d-flex flex-grow-1 mt-2 justify-content-end">
+                <ul className="d-flex flex-grow-1 mt-2 align-items-center justify-content-end">
                     <li>
                         <Link href="/about">
-                            <a className="nav-url mr-5">About</a>
+                            <a className="nav-url mr-5 text-white">About</a>
                         </Link>
                     </li>
 
                     <li>
                         <Link href="/projects">
-                            <a className="nav-url mr-5">Projects</a>
+                            <a className="nav-url mr-5 text-white">Projects</a>
                         </Link>
                     </li>
 
                     <li>
                         <Link href="/contact">
-                            <a className="nav-url">Contact</a>
+                            <a className="nav-url text-white">Contact</a>
                         </Link>
                     </li>
+                    <a href="https://github.com/erenustun/" className="ml-4 mb-2" target="_blank"><img src="/assets/images/github.png" height={26} /></a>
+                    <a href="https://www.linkedin.com/in/eren-üstün-957807153" className="ml-3 mb-2" target="_blank"><img src="/assets/images/linkedin.png" height={26} /></a>
                 </ul>
             </div>
         </header>

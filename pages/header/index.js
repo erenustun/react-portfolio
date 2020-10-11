e8f819b5b12fea74c1c8d357baf6387301f3cd8e
Eren <eren.uestuen@gmail.com>
2020-10-12 01:52:41 +0200

    Updated header style

--- pages/header/index.js
+++ pages/header/index.js
@@ -20,10 +20,11 @@ export default function Header() {
 
                     <li>
                         <Link href="/contact">
-                            <a className="nav-url text-white">Contact</a>
+                            <a className="nav-url text-white mr-3">Contact</a>
                         </Link>
                     </li>
-                    <a href="https://github.com/erenustun/" className="ml-4 mb-2" target="_blank"><img src="/assets/images/github.png" height={26} /></a>
+                    <div className="divider"></div>
+                    <a href="https://github.com/erenustun/" className="mb-2 ml-2" target="_blank"><img src="/assets/images/github.png" height={26} /></a>
                     <a href="https://www.linkedin.com/in/eren-üstün-957807153" className="ml-3 mb-2" target="_blank"><img src="/assets/images/linkedin.png" height={26} /></a>
                 </ul>
             </div>

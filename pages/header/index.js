37810f8eddf75a1576f00b3ee344a9bb96a2d89a
Eren <eren.uestuen@gmail.com>
2020-10-12 00:54:08 +0200

    Updated header component

--- pages/header/index.js
+++ pages/header/index.js
@@ -2,30 +2,29 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <header className="d-flex px-5">
-            <div className="shape"></div>
-            <div className="flex-grow-1">
-                <img src="assets/images/logo-white.png" height={100} className="mt-4"/>
+        <header className="d-flex align-items-center">
+            <div className="d-flex w-75 mx-auto align-items-center">
+                <h1 className="flex-grow-1 text-text-left m-0 mb-4 display-4 text-white">Eren.develops</h1>
+                <ul className="d-flex flex-grow-1 mt-2 justify-content-end">
+                    <li>
+                        <Link href="/about">
+                            <a className="nav-url mr-5">About</a>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/projects">
+                            <a className="nav-url mr-5">Projects</a>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/contact">
+                            <a className="nav-url">Contact</a>
+                        </Link>
+                    </li>
+                </ul>
             </div>
-            <ul className="d-flex flex-row flex-grow-1 align-items-center justify-content-end">
-                <li>
-                    <Link href="/about">
-                        <a className="nav-url">About</a>
-                    </Link>
-                </li>
-
-                <li>
-                    <Link href="/projects">
-                        <a className="nav-url">Projects</a>
-                    </Link>
-                </li>
-
-                <li>
-                    <Link href="/contact">
-                        <a className="nav-url">Contact</a>
-                    </Link>
-                </li>
-            </ul>
         </header>
     )
 }
